1a74820b90152407abff54e869183a364f98ea6d
Cronon <cronon12@gmail.com>
2014-02-25 01:04:18 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartReserve::Application.config.secret_key_base = '99171302414835f3b3279c012b8bf0c9612d6def83beec0d27416bea4b700702e4051712656e6e57cf5db042efcebfbee19aad8a6132440a7444e15111bd9053'
