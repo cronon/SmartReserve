8ab9dc8f472ad39b245ab8b96fd633817c9e54a0
Cronon <cronon12@gmail.com>
2014-02-25 16:30:24 +0300

    scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 SmartReserve::Application.routes.draw do
+  resources :orders
+
+  resources :tables
+
+  resources :clubs
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
