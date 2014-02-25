a7ad91ecfdc2316957a942b1ffcd7a00299d1f04
Cronon <cronon12@gmail.com>
2014-02-25 21:02:22 +0300

    rollbck

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -2,22 +2,4 @@ require 'spec_helper'
 
 describe Table do
 
-  before(:each) do
-    @attr = {
-      :name => "metropole",
-      :tables_count => "24",
-      :average_time => "300", # 300 sec == 5 min
-      #:description => "inka-chaka-zuma"
-    }
-  end
-
-  it "should create a new instance given a valid attribute" do
-    Club.create!(@attr)
-  end
-
-  it "should create a new tables" do
-    club=Club.create(@attr)
-    expect(club.table.length).to eq(24)
-  end
-
 end
