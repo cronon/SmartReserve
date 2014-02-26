d567942a565f5272cb149b794b16e66bde5753b6
Cronon <cronon12@gmail.com>
2014-02-26 18:07:00 +0300

    specs for table.will_free

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -55,4 +55,19 @@ describe Table do
     o = Order.new({:since=>Time.now+5.minutes, :until => Time.now+20.minutes})
     expect {@table.add_order(o)}.to raise_error(ActiveRecord::RecordInvalid)
   end 
+
+  it "should be free when big a lot of time between orders" do
+    @table.club = Club.new(:name => "Club",:average_time => 20.minutes,:tables_count => 10)
+    @table.add_order Order.new({:since=>Time.now, :until => Time.now+10.minutes})
+    @table.add_order Order.new({:since=>Time.now+10.days, :until => Time.now+10.days+10.minutes})
+    expect(@table.will_free(Time.now)).to eq(10.minutes)
+  end
+
+  it "shouldnt be free when small little of time between orders" do
+    @table.club = Club.new(:name => "Club",:average_time => 20.minutes,:tables_count => 10)
+    @table.add_order Order.new({:since=>Time.now, :until => Time.now+10.minutes})
+    @table.add_order Order.new({:since=>Time.now+15.minutes, :until => Time.now+50.minutes})
+    expect(@table.will_free(Time.now)).to eq(50.minutes)
+  end
+
 end
