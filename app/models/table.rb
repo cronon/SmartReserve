45845e2c9ed3ebb14a80e8b6074d7550add9048f
vitiaaap29 <viktor.kyrilovich@gmail.com>
2014-02-26 23:07:22 +0400

    will_free while unwork

--- app/models/table.rb
+++ app/models/table.rb
@@ -21,4 +21,39 @@ class Table < ActiveRecord::Base
     end
   end
 
+    def will_free(time_from_which_assume)
+    orders = sort_order_by_since(self.order)
+
+    puts "ORDERS = #{orders.size} empty? = #{orders.empty?}"
+
+    wrap_order = orders.find{ |order| time_from_which_assume < order.until}
+
+    return 0 if wrap_order.empty?
+    order = wrap_order.first
+
+    result = order.until - time_from_which_assume
+    if time_from_which_assume.between? order.since, order.until
+      next_index = orders.index order
+      if next_index < (orders.size - 1)
+        order_next = orders[next_index]
+        @interval = self.club.average_time
+        if order_next.since - order.until < @interval
+          result = order_next.until - time_from_which_assume
+        end
+      else
+        return 24*60.minute
+      end
+    end
+    result
+
+  end
+
+  private
+
+    def sort_order_by_since(orders)
+      arr_since = orders.map{ |order| order.since}.sort!
+      sorting_orders = arr_since.map{|s| orders.}
+
+    end
+
 end
