fd0e28b899ec9a625996f1248eb52d8265706134
vitiaaap29 <viktor.kyrilovich@gmail.com>
2014-02-26 17:20:04 +0400

    fix validates

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,16 +1,14 @@
 class Club < ActiveRecord::Base
   has_many :table, dependent: :destroy
-  #Тут закомменченно из-за того, что валидация не работает
-  #after_validation :create_tables
+  after_validation :create_tables
 
-  validate :tables_count, :name, presence: true
-	validate :tables_count, numericality: { grater_than_or_equal: 1 }
-	validate :tables_count, presence: true
+  validates :tables_count, :name, presence: true
+	validates :tables_count, numericality: { grater_than_or_equal: 1 }
 
- # protected
-    #def create_tables
-   #   self.tables_count.times do
-  #      self.table << Table.new
- #     end
-#    end
+  protected
+    def create_tables
+      self.tables_count.times do
+        self.table << Table.new
+      end
+    end
 end
