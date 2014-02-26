dd6b69c9d7422775a565e4d56330ec6b307ee626
Cronon <cronon12@gmail.com>
2014-02-26 16:29:01 +0300

    repair clubs and schema

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,27 +1,16 @@
 class Club < ActiveRecord::Base
   has_many :table, dependent: :destroy
-<<<<<<< HEAD
   before_save :create_tables
 
   validates :tables_count, :name, presence: true
 	validates :tables_count, numericality: { grater_than_or_equal: 1 }
 	validates :tables_count, presence: true
-=======
-  after_validation :create_tables
-
-  validates :tables_count, :name, presence: true
-	validates :tables_count, numericality: { grater_than_or_equal: 1 }
->>>>>>> fd0e28b899ec9a625996f1248eb52d8265706134
 
   protected
     def create_tables
       self.tables_count.times do
         self.table << Table.new
-<<<<<<< HEAD
-    end
-  end
-=======
       end
     end
->>>>>>> fd0e28b899ec9a625996f1248eb52d8265706134
-end
+  end
+
