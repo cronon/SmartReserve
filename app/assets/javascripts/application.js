3fbb1c259078cc4e393235726e846a3f953f363b
Cronon <cronon12@gmail.com>
2014-03-12 21:09:19 +0300

    photos uploads to club

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,4 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
-//= require jquery.datetimepicker
-//= require jquery.fileupload-ui
+// require jquery.datetimepicker
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,14 @@
 class PhotosController < ApplicationController
   #POST /clubs/2/photos
   def create
-    blog_image = Photo.new
-    blog_image.image = params[:photo_form][:uploaded_data]
-    blog_image.save!
+    photo = Photo.new
+    photo.image = params[:photo_form][:uploaded_data]
+    photo.club_id = params[:club_id]
+    photo.save!
  
-  # TODO: store blog_image.id in session OR pass ID back to form for storage in a hidden field
+  # TODO: store photo.id in session OR pass ID back to form for storage in a hidden field
   # OR if your main resource already exists, mount the uploader to it directly and go sip on a 
   # pina colada instead of worrying about this 
-    render :text => blog_image.image.url
+    render :text => photo.image.url
   end
 end
