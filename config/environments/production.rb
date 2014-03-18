1800a4b70256aaa5ba94b1e4fdfddb385ebec9ed
Cronon <cronon12@gmail.com>
2014-03-18 21:03:25 +0300

    fdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,4 +100,16 @@ SmartReserve::Application.configure do
   user_name: "real email on gmail from google",
   password: "real password,"
 }
+
+  config.action_mailer.default_url_options = { :host => 'http://agile-harbor-2406.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+  }
 end
