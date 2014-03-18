16bcf06cd64796dd6a8457f0bf936fbd1d8d32d6
Cronon <cronon12@gmail.com>
2014-03-18 21:10:21 +0300

    d

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,3 +113,13 @@ SmartReserve::Application.configure do
   :enable_starttls_auto => true
   }
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
