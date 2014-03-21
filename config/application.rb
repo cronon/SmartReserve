565a56f2f966f910c2959b4552a32430b61b66bd
Cronon <cronon12@gmail.com>
2014-03-21 22:16:03 +0300

    setupActive Admin, edited views

--- config/application.rb
+++ config/application.rb
@@ -6,18 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  # Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
-end
 
 module SmartReserve
   class Application < Rails::Application
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
