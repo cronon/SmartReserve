c5059bce5c2d21d8f531f9ee9879f8693809fbde
Cronon <cronon12@gmail.com>
2014-03-22 16:43:16 +0300

    set envvarsfor aws

--- config/initializers/carierwave.rb
+++ config/initializers/carierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_SECRET_KEY_ID'],                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
     # :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
     # :host                   => 's3.example.com',             # optional, defaults to nil
