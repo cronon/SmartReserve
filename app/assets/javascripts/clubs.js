cf761947ec3db5ef1b5f34d3ab02a8c14961122f
Cronon <cronon12@gmail.com>
2014-03-27 20:31:36 +0300

    fixed submit button in confitmation modal

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,3 +1,11 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+var postForm = function(form) {  
+  var valuesToSubmit = $(form).serialize();
+  $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'), //sumbits it to the given url of the form
+      data: valuesToSubmit
+  });
+}
