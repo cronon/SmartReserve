cc2da27a0cfd73b4289a16008883f57ab7a0338b
Cronon <cronon12@gmail.com>
2014-03-27 18:13:34 +0300

    d

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(:default, Rails.env)
 module SmartReserve
   class Application < Rails::Application
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
