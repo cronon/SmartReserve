e4e5bb027bb1849b678f7b01deced2eff273b70c
Cronon <cronon12@gmail.com>
2014-04-09 21:43:47 +0300

    remove aws to images.smartreserve.by

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,17 @@
 class PhotosController < ApplicationController
   #POST /clubs/2/photos
   def create
-    photo = Photo.new
-    photo.image = params[:file]
-    photo.club_id = params[:club_id]
-    photo.save!
+    @photo = Photo.new
+    @photo.image = params[:file]
+    @photo.club_id = params[:club_id]
+    @photo.save!
  
   # TODO: store photo.id in session OR pass ID back to form for storage in a hidden field
   # OR if your main resource already exists, mount the uploader to it directly and go sip on a 
   # pina colada instead of worrying about this 
-    @src = photo.image.url
+  end
+
+  def destroy    
+    @photo = Photo.find(params[:id]).destroy
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def to_http s#from https
+      'http://'+s[8..-1]
+    end
 end
