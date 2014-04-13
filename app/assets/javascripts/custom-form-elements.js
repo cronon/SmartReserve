443ce97edc687b8a1fe1e45ec1b56d2556c27b6b
Cronon <cronon12@gmail.com>
2014-04-13 15:47:18 +0300

    checkboxes doesnt work in chrome. Done all but popups

--- app/assets/javascripts/custom-form-elements.js
+++ app/assets/javascripts/custom-form-elements.js
@@ -23,7 +23,7 @@ Visit http://ryanfait.com/ for more information.
 
 */
 
-var checkboxHeight = "25";
+var checkboxHeight = "16";
 var radioHeight = "25";
 var selectWidth = "190";
 
@@ -141,4 +141,4 @@ var Custom = {
 		}
 	}
 }
-window.onload = Custom.init;
+$(window).load(function(){Custom.init()});
