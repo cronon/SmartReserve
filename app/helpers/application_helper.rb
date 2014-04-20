cecf5fe99595fae678c2df99578d0b40ae33d44f
Cronon <cronon12@gmail.com>
2014-04-20 13:59:31 +0300

    breadcrumbs and breadcrumbs helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,24 @@
 module ApplicationHelper
+
+  # breadcrumbs('->',{'Main'=>'/','Catalog'=>'/clubs','Stopline'=>'/15','Edit'=>'/edit'}) =>
+  # Main->Catalog->Stopline->Edit
+  def breadcrumbs separator, crumbs
+    content_tag(:span, class: 'breadcrumbs-span') do
+      url=''
+      crumbs.map do |name,link|
+        url += link
+        l=link_to sanitize(name), sanitize(url)
+        url = '' if url=='/'
+        l
+      end.join(separator).html_safe
+    end
+  end
+
   def to_http s#from https
     'http://'+s[8..-1]
   end
 
+  #dima:  try this -> time.strftime('%d.%m.%Y')
   #Wed, 16 Apr 2014 20:02:29 FET +03:00 to 16.04.2014
   def utc_to_year_mounth_day time
   	year_mounth_day = time.utc.to_s.split(" ").first
