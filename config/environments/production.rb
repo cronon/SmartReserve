c7fc927e30a9dd29ddeca13275edc64a14d7bb26
Cronon <cronon12@gmail.com>
2014-04-21 12:59:14 +0300

    timezones?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 SmartReserve::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.time_zone = 'Minsk'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
