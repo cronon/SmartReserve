e3aed5c84f08918150b4ab842ba710cdb63defaf
Kyrilovich Viktor <viktor.kyrilovich@gmail.com>
2014-05-09 20:49:09 +0300

    photo loader (start) on galery

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,19 +1,25 @@
 class PhotosController < ApplicationController
   #POST /clubs/photos
   def create
-    if params[:type] == "club"
-      #club = Club.find(params[:club_id])
-      #@photo = club.photos.build
-      @photo = Photo.create
-    else params[:type] == "news"
-      news = News.find(params[:news_id])
-      @photo = news.photos.build
+    if params[:club_id].nil?
+      if params[:type] == "club"
+        #club = Club.find(params[:club_id])
+        #@photo = club.photos.build
+        @photo = Photo.create
+      else params[:type] == "news"
+        news = News.find(params[:news_id])
+        @photo = news.photos.build
+      end
+      @photo.image = params[:file]
+      @photo.save!
+      session[:photos_for_req_club] ||= []
+      session[:photos_for_req_club] << @photo.id
+    else
+      club = Club.find(params[:club_id])
+      @photo = club.photos.build
+      @photo.image = params[:file]
+      @photo.save!
     end
-    @photo.image = params[:file]
-    @photo.save!
-    session[:photos_for_req_club] ||= []
-    session[:photos_for_req_club] << @photo.id
- 
   # TODO: store photo.id in session OR pass ID back to form for storage in a hidden field
   # OR if your main resource already exists, mount the uploader to it directly and go sip on a 
   # pina colada instead of worrying about this 
