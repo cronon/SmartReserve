0c993f841055259cce5f3d876f9c8ed395ca97bd
Cronon <cronon12@gmail.com>
2014-05-25 17:02:15 +0300

    admin invites

--- app/admin/invite.rb
+++ app/admin/invite.rb
@@ -1,3 +1,13 @@
-ActiveAdmin.register_page "Invites" do
-  # render :partial => "invites"
+ActiveAdmin.register Invite do
+  
+  permit_params :email, :phone, :name
+  form do |f|
+    f.inputs "User details" do
+      f.input :name
+      f.input :phone
+      f.input :email
+    end
+    f.actions
+  end
+  
 end
